66630285b21a2e8916ed087ddd7dead58729b54e
Ralph <ralph@huwiler.in>
2015-12-05 23:41:30 +0100

    Add Clockwork to only dev

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (!$this->app->environment('production')) {
+            $this->app->register(\Clockwork\Support\Laravel\ClockworkServiceProvider::class);
+        }
+
     }
 }
