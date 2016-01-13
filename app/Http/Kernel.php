d3b20fc2e8a1565d4de7d7f2c89972bea57459ab
Ralph <ralph@huwiler.in>
2016-01-13 20:23:37 +0100

    composer lock

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+
         \Maatwebsite\Sidebar\Middleware\ResolveSidebars::class,
         \SocietyCMS\Http\Middleware\VerifyCsrfToken::class,
 
